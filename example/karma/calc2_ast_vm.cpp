69c8aeb00802821ba5c32259e623b142ab29dd3b
Eric Niebler <eric@boostpro.com>
2008-05-08 21:41:48 +0000

    merged from trunk
    [SVN r45230]

--- example/karma/calc2_ast_vm.cpp
+++ example/karma/calc2_ast_vm.cpp
@@ -7,22 +7,24 @@
 =============================================================================*/
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  A Calculator example demonstrating generation of AST which gets dumped into
-//  a human readable format afterwards.
+//  A Calculator example demonstrating generation of AST from which we generate 
+//  a simple byte code representation being interpreted by a similar virtual 
+//  machine.
 //
 //  [ JDG April 28, 2008 ]
-//  [ HK April 28, 2008 ]
+//  [ HK May 05, 2008 ]
 //
 ///////////////////////////////////////////////////////////////////////////////
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/karma.hpp>
+#include <boost/spirit/home/karma/binary.hpp>
 
 #include <iostream>
 #include <vector>
 #include <string>
 
-#include "calc2_ast.hpp"
+#include "calc2_ast_vm.hpp"
 
 using namespace boost::spirit;
 using namespace boost::spirit::ascii;
@@ -62,39 +64,121 @@ struct calculator : qi::grammar_def<Iterator, expression_ast(), space_type>
 };
 
 ///////////////////////////////////////////////////////////////////////////////
+//  The Virtual Machine
+///////////////////////////////////////////////////////////////////////////////
+class vmachine
+{
+public:
+    union element {
+        int code;
+        char bytes[sizeof(int)];
+    };
+
+    vmachine(unsigned stackSize = 4096)
+      : stack(stackSize)
+      , stack_ptr(stack.begin())
+    {
+    }
+
+    int top() const { return stack_ptr[-1]; };
+    void execute(std::vector<element> const& code);
+
+private:
+    std::vector<int> stack;
+    std::vector<int>::iterator stack_ptr;
+};
+
+void vmachine::execute(std::vector<element> const& code)
+{
+    std::vector<element>::const_iterator pc = code.begin();
+    stack_ptr = stack.begin();
+
+    while ((*pc).code && pc != code.end())
+    {
+        switch ((*pc++).code)
+        {
+            case op_neg:
+                stack_ptr[-1] = -stack_ptr[-1];
+                break;
+
+            case op_add:
+                --stack_ptr;
+                stack_ptr[-1] += stack_ptr[0];
+                break;
+
+            case op_sub:
+                --stack_ptr;
+                stack_ptr[-1] -= stack_ptr[0];
+                break;
+
+            case op_mul:
+                --stack_ptr;
+                stack_ptr[-1] *= stack_ptr[0];
+                break;
+
+            case op_div:
+                --stack_ptr;
+                stack_ptr[-1] /= stack_ptr[0];
+                break;
+
+            case op_int:
+                *stack_ptr++ = (*pc++).code;
+                break;
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
 //  Our AST grammar for the generator, this just dumps the AST as a expression
 ///////////////////////////////////////////////////////////////////////////////
-template <typename OuputIterator>
-struct dump_ast
-  : karma::grammar_def<OuputIterator, expression_ast(), space_type>
+template <typename OuputIterator, typename Delimiter>
+struct generate_byte_code
+  : karma::grammar_def<OuputIterator, expression_ast(), Delimiter>
 {
-    dump_ast()
+    generate_byte_code()
     {
         ast_node %= 
-                int_        [_1 = _int(_val)]
-            |   binary_node [_1 = _bin_op(_val)]
-            |   unary_node  [_1 = _unary_op(_val)]
+                (dword(op_int) << dword) [_1 = _int(_val)]
+            |   binary_node              [_1 = _bin_op(_val)]
+            |   unary_node               [_1 = _unary_op(_val)]
             ;
             
         binary_node = 
-                ('(' << ast_node << char_ << ast_node << ')')
+                (ast_node << ast_node << byte)
                 [ 
-                    _1 = _left(_val), _2 = _op(_val), _3 = _right(_val)
+                    _1 = _left(_val), _2 = _right(_val), _3 = _op(_val)
                 ]
             ;
 
         unary_node =
-                ('(' << char_ << ast_node << ')')
+                (ast_node << byte)
                 [
-                    _1 = _op(_val), _2 = _right(_val)
+                    _1 = _right(_val), _2 = _op(_val)
                 ]
             ;
     }
 
-    karma::rule<OuputIterator, expression_ast(), space_type> ast_node;
-    karma::rule<OuputIterator, binary_op(), space_type> binary_node;
-    karma::rule<OuputIterator, unary_op(), space_type> unary_node;
+    karma::rule<OuputIterator, expression_ast(), Delimiter> ast_node;
+    karma::rule<OuputIterator, binary_op(), Delimiter> binary_node;
+    karma::rule<OuputIterator, unary_op(), Delimiter> unary_node;
 };
+
+///////////////////////////////////////////////////////////////////////////////
+// helper function helping to deduce the delimiter type
+template <typename Delimiter>
+bool generate_vm_code(expression_ast const& ast, 
+    std::vector<vmachine::element>& code, Delimiter const& d)
+{
+    // Our generator grammar definitions
+    typedef char* output_iterator_type;
+    typedef generate_byte_code<output_iterator_type, Delimiter> generate_byte_code;
+    
+    generate_byte_code generate_byte_code_def;
+    karma::grammar<generate_byte_code> gen_vm(
+        generate_byte_code_def, generate_byte_code_def.ast_node); 
+
+    return karma::generate_delimited((*code.begin()).bytes, gen_vm, ast, d);
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 //  Main program
@@ -103,7 +187,7 @@ int
 main()
 {
     std::cout << "/////////////////////////////////////////////////////////\n\n";
-    std::cout << "Dump AST's for simple expressions...\n\n";
+    std::cout << "Compile simple expressions to bytecode...\n\n";
     std::cout << "/////////////////////////////////////////////////////////\n\n";
     std::cout << "Type an expression...or [q or Q] to quit\n\n";
 
@@ -113,13 +197,6 @@ main()
 
     calculator def; 
     qi::grammar<calculator> calc(def, def.expression);
-
-    // Our generator grammar definitions
-    typedef std::back_insert_iterator<std::string> output_iterator_type;
-    typedef dump_ast<output_iterator_type> dump_ast;
-    
-    dump_ast dump_ast_def;
-    karma::grammar<dump_ast> ast_grammar(dump_ast_def, dump_ast_def.ast_node); 
 
     std::string str;
     while (std::getline(std::cin, str))
@@ -134,14 +211,16 @@ main()
 
         if (r && iter == end)
         {
-            std::string generated;
-            r = karma::generate_delimited(
-                std::back_inserter(generated), ast_grammar, ast, space);
+            // we assume a vm code size of 4096 is sufficient
+            std::vector<vmachine::element> code (4096);
+            r = generate_vm_code(ast, code, pad(4));
             
             if (r)
             {
-                std::cout << "AST for '" << str << "': " << generated 
-                          << std::endl;
+                vmachine vm;
+                vm.execute(code);
+                std::cout << "\nresult = " << vm.top() << std::endl;
+                std::cout << "-------------------------\n";
             }
             else
             {
