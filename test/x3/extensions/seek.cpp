4e3911c3744e5d53d1afb9986cd466bc00072c4e
Lee Clagett <code@leeclagett.com>
2014-04-14 23:08:53 -0400

    Added the seek keyword from spirit v2 repository to x3 extensions.
    This patch does not test semantic actions with the seek parser - the
    feature is not yet implemented/finalized in x3. All other tests from v2
    have been moved.

---
+++ test/x3/extensions/seek.cpp
@@ -0,0 +1,83 @@
+/*//////////////////////////////////////////////////////////////////////////////
+    Copyright (c) 2011 Jamboree
+    Copyright (c) 2014 Lee Clagett
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//////////////////////////////////////////////////////////////////////////////*/
+
+
+#include <vector>
+
+#include <boost/detail/lightweight_test.hpp>
+
+#include <boost/spirit/home/x3/auxiliary/eoi.hpp>
+#include <boost/spirit/home/x3/core.hpp>
+#include <boost/spirit/home/x3/char.hpp>
+#include <boost/spirit/home/x3/string.hpp>
+#include <boost/spirit/home/x3/numeric.hpp>
+#include <boost/spirit/home/x3/operator/plus.hpp>
+#include <boost/spirit/home/x3/operator/sequence.hpp>
+
+#include <boost/spirit/home/x3/extensions/directive/seek.hpp>
+
+#include "test.hpp"
+
+
+///////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    using namespace spirit_test;
+    namespace x3 = boost::spirit::x3;
+
+    // test eoi
+    {
+        BOOST_TEST(test("", x3::seek[x3::eoi]));
+        BOOST_TEST(test(" ", x3::seek[x3::eoi], x3::space));
+        BOOST_TEST(test("a", x3::seek[x3::eoi]));
+        BOOST_TEST(test(" a", x3::seek[x3::eoi], x3::space));
+    }
+
+    // test literal finding
+    {
+        int i = 0;
+
+        BOOST_TEST(
+            test_attr("!@#$%^&*KEY:123", x3::seek["KEY:"] >> x3::int_, i)
+            && i == 123
+        );
+    }
+    // test sequence finding
+    {
+        int i = 0;
+
+        BOOST_TEST(
+            test_attr("!@#$%^&* KEY : 123", x3::seek[x3::lit("KEY") >> ':'] >> x3::int_, i, x3::space)
+            && i == 123
+        );
+    }
+
+    // test attr finding
+    {
+        std::vector<int> v;
+
+        BOOST_TEST( // expect partial match
+            test_attr("a06b78c3d", +x3::seek[x3::int_], v, false)
+            && v.size() == 3 && v[0] == 6 && v[1] == 78 && v[2] == 3
+        );
+    }
+
+    // test action
+    {
+        /* action does not appear to be implemented in x3 yet
+           bool b = false;
+
+       BOOST_TEST( // expect partial match
+           test("abcdefg", seek["def"][ref(b) = true], false)
+              && b
+       );
+        */
+    }
+
+    return boost::report_errors();
+}
